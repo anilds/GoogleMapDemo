89613557910b1aef4e9c9b374e6f5e2d77f1891b
anil <anil@home-computer>
2014-11-12 17:53:21 +0530

    Second Change.

--- src/com/example/googlemap/MainActivity.java
+++ src/com/example/googlemap/MainActivity.java
@@ -7,18 +7,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.maps.MapActivity;
+
 
 import android.os.Bundle;
-import android.app.Activity;
+
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
+
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
-import android.widget.Toast;
-
 
 	public class MainActivity extends FragmentActivity {
 		 
@@ -41,32 +39,13 @@ import android.widget.Toast;
 	 
 	    }
 	 
-	    /**
-	     * function to load map. If map is not created it will create it for you
-	     * */
 	    private void initilizeMap() {
-	       /* if (googleMap == null) {
-	            googleMap = ((MapFragment) getFragmentManager().findFragmentById (R.id.map)).getMap();
-	            Marker hamburg = googleMap.addMarker(new MarkerOptions().position(BANGALORE)
-	                    .title("Bangalore"));
-	         //Move the camera instantly to hamburg with a zoom of 15.
-	            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(BANALORE, 15));
-
-	            // Zoom in, animating the camera.
-	            googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
-	            // check if map is created successfully or not
-	            if (googleMap == null) {
-	                Toast.makeText(getApplicationContext(),
-	                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
-	                        .show();
-	            }
-	        }
-	        */
+	       
 	    	SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
 	        googleMap = mapFragment.getMap();
 	        Marker bangalore = googleMap.addMarker(new MarkerOptions().position(BANGALORE)
                     .title("Bangalore"));
-	      //Move the camera instantly to hamburg with a zoom of 15.
+	     
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(BANGALORE, 15));
 
             // Zoom in, animating the camera.
