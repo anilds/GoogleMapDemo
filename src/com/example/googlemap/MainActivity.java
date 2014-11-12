acffe31e3e68b8a88a70c0ebab1a06813fb74a7b
anil <anil@home-computer>
2014-11-12 17:47:47 +0530

    Second Commit

--- src/com/example/googlemap/MainActivity.java
+++ src/com/example/googlemap/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.googlemap;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -10,15 +11,20 @@ import com.google.android.maps.MapActivity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.Toast;
 
 
-	public class MainActivity extends Activity {
+	public class MainActivity extends FragmentActivity {
 		 
 	    // Google Map
 	    private GoogleMap googleMap;
-	    static final LatLng BANALORE = new LatLng(12.985112,77.589741);
+	    static final LatLng BANGALORE = new LatLng(12.985112,77.589741);
 	 
 	    @Override
 	    protected void onCreate(Bundle savedInstanceState) {
@@ -39,9 +45,9 @@ import android.widget.Toast;
 	     * function to load map. If map is not created it will create it for you
 	     * */
 	    private void initilizeMap() {
-	        if (googleMap == null) {
+	       /* if (googleMap == null) {
 	            googleMap = ((MapFragment) getFragmentManager().findFragmentById (R.id.map)).getMap();
-	            Marker hamburg = googleMap.addMarker(new MarkerOptions().position(BANALORE)
+	            Marker hamburg = googleMap.addMarker(new MarkerOptions().position(BANGALORE)
 	                    .title("Bangalore"));
 	         //Move the camera instantly to hamburg with a zoom of 15.
 	            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(BANALORE, 15));
@@ -55,6 +61,36 @@ import android.widget.Toast;
 	                        .show();
 	            }
 	        }
+	        */
+	    	SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+	        googleMap = mapFragment.getMap();
+	        Marker bangalore = googleMap.addMarker(new MarkerOptions().position(BANGALORE)
+                    .title("Bangalore"));
+	      //Move the camera instantly to hamburg with a zoom of 15.
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(BANGALORE, 15));
+
+            // Zoom in, animating the camera.
+            googleMap.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+	        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+	 
+	        RadioGroup rgViews = (RadioGroup) findViewById(R.id.rg_views);
+	 
+	        rgViews.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+	 
+	            @Override
+	            public void onCheckedChanged(RadioGroup group, int checkedId) {
+	                if(checkedId == R.id.rb_normal){
+	                    googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+	                }else if(checkedId == R.id.rb_satellite){
+	                    googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+	                }else if(checkedId == R.id.rb_terrain){
+	                    googleMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+	                }
+	            }
+	        });
+	        
+	 
+
 	    }
 	 
 	    @Override
@@ -62,5 +98,15 @@ import android.widget.Toast;
 	        super.onResume();
 	        initilizeMap();
 	    }
+	    
+	    @Override
+	    public boolean onCreateOptionsMenu(Menu menu) {
+	    	// TODO Auto-generated method stub
+	    	MenuInflater menuInflater = getMenuInflater();
+	    	menuInflater.inflate(R.menu.activity_main, menu);
+	    	return true;
+	    }
+
+	    
 	 
 	}
